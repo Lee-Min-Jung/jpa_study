35762dd796a79662ccbb02ca073a97efad4cae83
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-02-10 10:38:50 +0900

    member 엔티티 수정

---
+++ src/main/java/com/study/jpa/jpa_study/entity/Member.java
@@ -0,0 +1,37 @@
+package com.study.jpa.jpa_study.entity;
+
+import com.study.jpa.jpa_study.enums.RoleType;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="member")
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @Column(name="id")
+    private String id;
+
+    @Column(name="name")
+    private String username;
+
+    @Column(name="age")
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+}
